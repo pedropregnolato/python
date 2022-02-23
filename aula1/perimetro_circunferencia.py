ea2033b73bba464e488d6afbd4aae02ede795848
Pedro Lucas Nunes Pregnolato <pregnolato.pedro@hcosta.com.br>
2022-02-22 21:55:57 -0300

    exercicios aula 1

---
+++ aula1/perimetro_circunferencia.py
@@ -0,0 +1,8 @@
+''' Faça um programa que exiba o perímetro de uma circunferência a partir do seu raio. '''
+
+import math
+
+raio = float(input("Raio: "))
+conta = (2 * math.pi) * raio
+
+print(conta)
