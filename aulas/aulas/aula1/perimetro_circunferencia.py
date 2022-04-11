8c9ee88d67d8c4b6272688caa110980c2048c788
pedropregnolato <pedrolucaspregnolato@gmail.com>
2022-04-11 00:25:23 -0300

    organização

--- aulas/aulas/aula1/perimetro_circunferencia.py
+++
@@ -1,8 +0,0 @@
-''' Faça um programa que exiba o perímetro de uma circunferência a partir do seu raio. '''
-
-import math
-
-raio = float(input("Raio: "))
-conta = (2 * math.pi) * raio
-
-print(conta)
